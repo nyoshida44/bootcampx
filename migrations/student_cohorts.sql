295c5ac885fba3c454a00baf6e9fd84895fc62e9
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-22 19:33:30 +0000

    First SQL exercise for bootcampx

---
+++ migrations/student_cohorts.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS cohorts;
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT,
+  phone TEXT,
+  github TEXT,
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
